66018de46052e48eb309e902eebe2bf5948fa114
aviral <aviral.gupta@xenondigilabs.com>
2021-06-06 13:40:15 +0530

    Added testing change in app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Testing 1
+         Testing 3
         </p>
         <a
           className="App-link"
